fdfa63b1d02876f5df385db0faae873d760362f0
Guillaume La Roque <glaroque@baylibre.com>
2022-05-24 16:58:41 +0200

    camera: Enable USB camera support
    Add packages and file to support USB camera with default
    implementation.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- device.mk
+++ device.mk
@@ -255,6 +255,12 @@ PRODUCT_COPY_FILES += \
         frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
         frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Enable USB Camera
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service
+PRODUCT_COPY_FILES += \
+    device/ti/am62x/camera/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 PRODUCT_PACKAGES += \
         Launcher3QuickStep \
         WallpaperPicker
