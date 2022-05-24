2f5297d5a80082abc5b2b922ae0c3618b2294d26
Nicolas Frayer <nfrayer@baylibre.com>
2022-05-24 17:56:33 +0200

    wifi: Add wpa_supplicants and init rc
    Add wpa_supplicants configuration files as well as adding
    init configuration
    
    Signed-off-by: Nicolas Frayer <nfrayer@baylibre.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,12 @@ PRODUCT_PACKAGES += \
         Launcher3QuickStep \
         WallpaperPicker
 
+# Wifi configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
+
 # Include hardware projects (HALs)
 $(call inherit-product-if-exists, hardware/ti/am62x/am62x.mk)
 
