6050a437dd742dce7fbfd45d1d99947788e5e817
Guillaume La Roque <glaroque@baylibre.com>
2022-06-29 23:38:25 +0000

    device: move on last fastboot HAL
    update to fastboot HAL 1.1
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- device.mk
+++ device.mk
@@ -73,8 +73,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
 PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0 \
-	android.hardware.fastboot@1.0-impl-mock \
+	android.hardware.fastboot@1.1 \
+	android.hardware.fastboot@1.1-impl-mock \
 	fastbootd
 
 # All VNDK libraries (HAL interfaces, VNDK, VNDK-SP, LL-NDK)
