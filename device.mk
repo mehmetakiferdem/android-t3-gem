3fabdd66ed41f7597f81f527cc18094f2f329e2e
Guillaume La Roque <glaroque@baylibre.com>
2022-08-04 21:53:16 +0000

    media: Add media performance file
    Add media performance files.
    This files was generated with reulst from CtsVideoTestCases then with
    result we use ./cts/tools/cts-media/get_achievable_rates.py.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- device.mk
+++ device.mk
@@ -268,6 +268,10 @@ PRODUCT_COPY_FILES += \
         frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
         frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Media configuration
+PRODUCT_COPY_FILES += \
+        device/ti/am62x/am62x.media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+
 # Enable USB Camera
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service
