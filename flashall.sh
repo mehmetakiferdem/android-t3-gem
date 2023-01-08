mehmetakiferdem/android-t3-gem

commit 2c5e4414279022265f947b22145c3baad9584564

am62x|am62x-lp: add support of SD Card boot
Implement support of SD card to flash android instead of EMMC.
- separate selinux rules to support mmc and sdcard , not same mmcblock
  number
- update flashall script to support sdcard flashing
- create fstab for sdcard with good mncblock path
- add TARGET_SDCARD_BOOT build flag to generate image for SD Card instead of
  EMMC

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

flashall.sh
@@ -1,166 +1,226 @@
 #!/bin/bash
+set -e
+set -u
+set -o pipefail
 
-usage ()
-{
+function usage {
 	echo "Usage: sudo flashall.sh <options>";
 	echo "options:";
 	echo "  --help Show this message and exit"
 	exit 1;
 }
 
-#no args case
-if [ "$1" = "--help" ] ; then
-	usage
-fi
+function main {
+	local opts_args="sdcard:,help"
+	local opts=$(getopt -o '' -l "${opts_args}" -- "$@")
+	eval set -- "${opts}"
 
-# Pre-packaged DB
-export FASTBOOT=${FASTBOOT-$(which fastboot)}
-export PRODUCT_OUT=${PRODUCT_OUT-"./"}
-export LD_LIBRARY_PATH=./
+	local sd_dev=""
 
-echo "Fastboot: $FASTBOOT"
-echo "Image location: $PRODUCT_OUT"
+	while true; do
+		case "$1" in
+			--sdcard) sd_dev="$2"; shift 2 ;;
+			--help) usage; exit 0 ;;
+			--) shift; break ;;
+		esac
+	done
 
+	if  ! [ -z "${sd_dev}" ]; then
+		if [ "$EUID" -ne 0 ]
+		then echo "Please run as root/sudo"
+		exit
+		fi
 
-# =============================================================================
-# pre-run
-# =============================================================================
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << FDISK_CMDS  | fdisk ${sd_dev}
+g      # create new GPT partition
+n      # add new partition
+2      # partition number
+8192	# first sector 
++1MiB # partition size
+n      # add new partition
+1      # partition number
+10240	# default - first sector 
++8Mib	# default - last sector
+w      # write partition table and exit
+FDISK_CMDS
 
-# Verify fastboot program is available
-# Verify user permission to run fastboot
-# Verify fastboot detects a device, otherwise exit
-if [ -f ${FASTBOOT} ]; then
-	fastboot_status=`${FASTBOOT} devices 2>&1`
-	if [ `echo $fastboot_status | grep -wc "no permissions"` -gt 0 ]; then
-		cat <<-EOF >&2
-		-------------------------------------------
-		 Fastboot requires administrator permissions
-		 Please run the script as root or create a
-		 fastboot udev rule, e.g:
+		parted --align optimal --script  ${sd_dev} \
+			name 2 tiboot3 \
+			name 1 bootloader
 
-		 % cat /etc/udev/rules.d/99_android.rules
-		   SUBSYSTEM=="usb",
-		   SYSFS{idVendor}=="0451"
-		   OWNER="<username>"
-		   GROUP="adm"
-		-------------------------------------------
-		EOF
-		exit 1
-	elif [ "X$fastboot_status" = "X" ]; then
-		echo "No device detected. Please ensure that" \
-			 "fastboot is running on the target device"
-                exit -1;
-	else
-		device=`echo $fastboot_status | awk '{print$1}'`
-		echo -e "\nFastboot - device detected: $device\n"
+		dd if=tiboot3.bin of=${sd_dev}2 
+		sync
+		mkfs.vfat -F 32 -n "bootloader" "${sd_dev}1"
+		mkdir boot
+		mount  ${sd_dev}1 boot/
+		cp ti* u-boot.img boot/
+		umount boot
+		rm -r boot
+		echo "Insert SD card on board, Power ON and interrupt U-Boot to go in console to do this command:"
+		echo "=> mmc dev 0 0"
+		echo "=> mmc erase 0 0x10000"
+		echo "=> mmc dev 0 1"
+		echo "=> mmc erase 0 0x10000"
+		echo "=> env default -a"
+		echo "=> run set_android_boot; setenv mmcdev 1; saveenv; reset;"
+		echo " Interrupt U-boot  to go in console:"
+		echo "=> fastboot 0"
+		echo "When it's Done"
+		read -p "Press any key to continue... " -n1 -s
 	fi
-else
-	echo "Error: fastboot is not available at ${FASTBOOT}"
-        exit -1;
+		dd if=/dev/zero of=bootloader.img bs=1048576 count=8
+		mkfs.vfat bootloader.img
+		mcopy -i bootloader.img tispl.bin ::tispl.bin
+		mcopy -i bootloader.img u-boot.img ::u-boot.img
+		# Pre-packaged DB
+		export FASTBOOT=${FASTBOOT-$(which fastboot)}
+		export PRODUCT_OUT=${PRODUCT_OUT-"./"}
+		export LD_LIBRARY_PATH=./
+
+		echo "Fastboot: $FASTBOOT"
+		echo "Image location: $PRODUCT_OUT"
+
+
+		# =============================================================================
+		# pre-run
+		# =============================================================================
+
+		# Verify fastboot program is available
+		# Verify user permission to run fastboot
+		# Verify fastboot detects a device, otherwise exit
+		if [ -f ${FASTBOOT} ]; then
+			fastboot_status=`${FASTBOOT} devices 2>&1`
+			if [ `echo $fastboot_status | grep -wc "no permissions"` -gt 0 ]; then
+				cat <<-EOF >&2
+				-------------------------------------------
+				Fastboot requires administrator permissions
+				Please run the script as root or create a
+				fastboot udev rule, e.g:
+
+				% cat /etc/udev/rules.d/99_android.rules
+				SUBSYSTEM=="usb",
+				SYSFS{idVendor}=="0451"
+				OWNER="<username>"
+				GROUP="adm"
+				-------------------------------------------
+				EOF
+				exit 1
+			elif [ "X$fastboot_status" = "X" ]; then
+				echo "No device detected. Please ensure that" \
+					"fastboot is running on the target device"
+						exit -1;
+			else
+				device=`echo $fastboot_status | awk '{print$1}'`
+				echo -e "\nFastboot - device detected: $device\n"
+			fi
+		else
+			echo "Error: fastboot is not available at ${FASTBOOT}"
+				exit -1;
+		fi
+
+		## poll the board to find out its configuration
+		cpu=`${FASTBOOT} getvar cpu 2>&1         | grep cpu     | awk '{print$2}'`
+		cputype=`${FASTBOOT} getvar secure 2>&1  | grep secure  | awk '{print$2}'`
+		boardrev=`${FASTBOOT} getvar board_rev 2>&1  | grep board_rev  | awk '{print$2}' | cut -b 1`
+
+		# Create the filename
+		tiboot3bin="${PRODUCT_OUT}tiboot3.bin"
+		bootloaderimg="${PRODUCT_OUT}bootloader.img"
+		userdataimg="${PRODUCT_OUT}userdata.img"
+		superimg="${PRODUCT_OUT}super.img"
+		bootimg="${PRODUCT_OUT}boot.img"
+		vbmetaimg="${PRODUCT_OUT}vbmeta.img"
+		dtboimg="${PRODUCT_OUT}dtbo.img"
+		dtbouimg="${PRODUCT_OUT}dtbo-unsigned.img"
+		persistimg="${PRODUCT_OUT}persist.img"
+
+		# Verify that all the files required for the fastboot flash
+		# process are available
+
+		if [ ! -e "${tiboot3bin}" ] ; then
+		echo "Missing ${tiboot3bin}"
+		exit -1;
+		fi
+		if [ ! -e "${bootloaderimg}" ] ; then
+		echo "Missing ${bootloaderimg}"
+		exit -1;
+		fi
+		if [ ! -e "${superimg}" ] ; then
+		echo "Missing ${superimg}"
+		exit -1;
+		fi
+		if [ ! -e "${userdataimg}" ] ; then
+		echo "Missing ${userdataimg}"
+		exit -1;
+		fi
+		if [ ! -e "${bootimg}" ] ; then
+		echo "Missing ${bootimg}"
+		exit -1;
+		fi
+		if [ ! -e "${persistimg}" ] ; then
+		echo "Missing ${persistimg}"
+		exit -1;
+		fi
+
+		echo "Create GPT partition table"
+		${FASTBOOT} oem format
+
+		sleep 3
+
+		echo "Flashing tiboot3....."
+		echo "   tiboot3bin:     ${tiboot3bin}"
+		${FASTBOOT} flash tiboot3	${tiboot3bin}
+
+		sleep 3
+		echo "   bootloader:  ${bootloaderimg}"
+		${FASTBOOT} flash bootloader	${bootloaderimg}
+
+		echo "Flash android partitions"
+		${FASTBOOT} flash super	${superimg}
+
+		echo "Flashing Boot Image"
+		${FASTBOOT} flash boot_a ${bootimg}
+		${FASTBOOT} flash boot_b ${bootimg}
+
+		echo "Flashing userdata Image"
+		${FASTBOOT} flash userdata ${userdataimg}
+
+		if [ -e "${vbmetaimg}" ] ; then
+			if [ ! -e "${dtboimg}" ] ; then
+				echo "Missing ${dtboimg}"
+				exit -1;
+			fi
+			echo "Flashing vbmeta Image"
+			${FASTBOOT} flash vbmeta_a ${vbmetaimg}
+			${FASTBOOT} flash vbmeta_b ${vbmetaimg}
+			echo "Flashing DTBO Image"
+			${FASTBOOT} flash dtbo_a ${dtboimg}
+			${FASTBOOT} flash dtbo_b ${dtboimg}
+		else
+			if [ ! -e "${dtbouimg}" ] ; then
+			echo "Missing ${dtbouimg}"
+			exit -1;
+			fi
+			echo "Flashing DTBO Image Unsigned"
+			${FASTBOOT} flash dtbo_a ${dtbouimg}
+			${FASTBOOT} flash dtbo_b ${dtbouimg}
+		fi
+
+		echo "Flashing persist partition"
+		${FASTBOOT} flash persist ${persistimg}
+
+		echo "Erasing misc partitions"
+		${FASTBOOT} erase misc
+
+		echo "Formatting metadata partition"
+		${FASTBOOT} format metadata
+		if [ $? -eq 1 ]; then
+			echo "formating failed"
+			${FASTBOOT} erase metadata
+
+		fi
+}
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+    main "$@"
 fi
-
-# make bootloader image
-# bootloader.img is FAT Image consisting
-# u-boot.img and tispl.bin
-dd if=/dev/zero of=bootloader.img bs=1048576 count=8
-mkfs.vfat bootloader.img
-mcopy -i bootloader.img tispl.bin ::tispl.bin
-mcopy -i bootloader.img u-boot.img ::u-boot.img
-
-## poll the board to find out its configuration
-cpu=`${FASTBOOT} getvar cpu 2>&1         | grep cpu     | awk '{print$2}'`
-cputype=`${FASTBOOT} getvar secure 2>&1  | grep secure  | awk '{print$2}'`
-boardrev=`${FASTBOOT} getvar board_rev 2>&1  | grep board_rev  | awk '{print$2}' | cut -b 1`
-
-# Create the filename
-tiboot3bin="${PRODUCT_OUT}tiboot3.bin"
-bootloaderimg="${PRODUCT_OUT}bootloader.img"
-userdataimg="${PRODUCT_OUT}userdata.img"
-superimg="${PRODUCT_OUT}super.img"
-bootimg="${PRODUCT_OUT}boot.img"
-vbmetaimg="${PRODUCT_OUT}vbmeta.img"
-persistimg="${PRODUCT_OUT}persist.img"
-dtboimg="${PRODUCT_OUT}dtbo.img"
-dtbouimg="${PRODUCT_OUT}dtbo-unsigned.img"
-
-# Verify that all the files required for the fastboot flash
-# process are available
-
-if [ ! -e "${tiboot3bin}" ] ; then
-  echo "Missing ${tiboot3bin}"
-  exit -1;
-fi
-if [ ! -e "${bootloaderimg}" ] ; then
-  echo "Missing ${bootloaderimg}"
-  exit -1;
-fi
-if [ ! -e "${superimg}" ] ; then
-  echo "Missing ${superimg}"
-  exit -1;
-fi
-if [ ! -e "${userdataimg}" ] ; then
-  echo "Missing ${userdataimg}"
-  exit -1;
-fi
-if [ ! -e "${bootimg}" ] ; then
-  echo "Missing ${bootimg}"
-  exit -1;
-fi
-if [ ! -e "${persistimg}" ] ; then
-  echo "Missing ${persistimg}"
-  exit -1;
-fi
-
-echo "Create GPT partition table"
-${FASTBOOT} oem format
-
-sleep 3
-
-echo "Flashing tiboot3....."
-echo "   tiboot3bin:     ${tiboot3bin}"
-${FASTBOOT} flash tiboot3	${tiboot3bin}
-
-sleep 3
-echo "   bootloader:  ${bootloaderimg}"
-${FASTBOOT} flash bootloader	${bootloaderimg}
-
-echo "Flash android partitions"
-${FASTBOOT} flash super	${superimg}
-
-echo "Flashing Boot Image"
-${FASTBOOT} flash boot_a ${bootimg}
-${FASTBOOT} flash boot_b ${bootimg}
-
-echo "Flashing userdata Image"
-${FASTBOOT} flash userdata ${userdataimg}
-
-if [ -e "${vbmetaimg}" ] ; then
-	if [ ! -e "${dtboimg}" ] ; then
-  		echo "Missing ${dtboimg}"
-  		exit -1;
-	fi
-	echo "Flashing vbmeta Image"
-  	${FASTBOOT} flash vbmeta_a ${vbmetaimg}
-  	${FASTBOOT} flash vbmeta_b ${vbmetaimg}
-  	echo "Flashing DTBO Image"
-	${FASTBOOT} flash dtbo_a ${dtboimg}
-	${FASTBOOT} flash dtbo_b ${dtboimg}
-else
-	if [ ! -e "${dtbouimg}" ] ; then
-	echo "Missing ${dtbouimg}"
-	exit -1;
-	fi
-	echo "Flashing DTBO Image Unsigned"
-	${FASTBOOT} flash dtbo_a ${dtbouimg}
-	${FASTBOOT} flash dtbo_b ${dtbouimg}
-fi
-
-echo "Flashing persist partition"
-${FASTBOOT} flash persist ${persistimg}
-
-echo "Erasing misc partitions"
-${FASTBOOT} erase misc
-
-echo "Formatting metadata partition"
-${FASTBOOT} format metadata
-

BoardConfig.mk
@@ -105,7 +105,11 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version 2
 BOARD_KERNEL_CMDLINE += no_console_suspend console=ttyS2,115200 
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on androidboot.selinux=permissive
+ifeq ($(TARGET_SDCARD_BOOT), true)
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=bus@f0000/fa00000.mmc
+else
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=bus@f0000/fa10000.mmc
+endif
 BOARD_KERNEL_CMDLINE += init=/init
 BOARD_KERNEL_CMDLINE += cma=512M
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
@@ -118,7 +122,12 @@ endif
 
 DEVICE_MANIFEST_FILE := device/ti/am62x/manifest.xml
 
-BOARD_SEPOLICY_DIRS += device/ti/am62x/sepolicy
+BOARD_SEPOLICY_DIRS += device/ti/am62x/sepolicy/common/
+ifeq ($(TARGET_SDCARD_BOOT), true)
+BOARD_SEPOLICY_DIRS += device/ti/am62x/sepolicy/sdcard/
+else
+BOARD_SEPOLICY_DIRS += device/ti/am62x/sepolicy/mmc/
+endif
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/ti/am62x/sepolicy-private
 
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true

device.mk
@@ -264,6 +264,17 @@ PRODUCT_COPY_FILES += \
         device/ti/am62x/ueventd.am62x.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 # fstab
+ifeq ($(TARGET_SDCARD_BOOT), true)
+ifeq ($(TARGET_AVB_ENABLE), true)
+PRODUCT_COPY_FILES += \
+    device/ti/am62x/fstab.am62x.avb.sdcard:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.am62x \
+    device/ti/am62x/fstab.am62x.avb.sdcard:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.am62x
+else
+PRODUCT_COPY_FILES += \
+    device/ti/am62x/fstab.am62x.sdcard:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.am62x \
+    device/ti/am62x/fstab.am62x.sdcard:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.am62x
+endif
+else
 ifeq ($(TARGET_AVB_ENABLE), true)
 PRODUCT_COPY_FILES += \
     device/ti/am62x/fstab.am62x.avb:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.am62x \
@@ -273,7 +284,7 @@ PRODUCT_COPY_FILES += \
     device/ti/am62x/fstab.am62x:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.am62x \
     device/ti/am62x/fstab.am62x:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.am62x
 endif
-
+endif
 # RecoveryOS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.recovery.am62x.rc:recovery/root/vendor/etc/init/init.recovery.am62x.rc

fstab.am62x.avb.sdcard
@@ -1,0 +1,9 @@
+system                                                       /system         ext4    ro,barrier=1                     wait,avb=vbmeta,slotselect,logical,first_stage_mount
+vendor                                                       /vendor         ext4    ro,barrier=1                     wait,avb,slotselect,logical,first_stage_mount
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/metadata   /metadata       ext4    noatime,nosuid,nodev,discard     wait,check,formattable,first_stage_mount
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/userdata   /data           f2fs    noatime,nosuid,nodev,discard,reserve_root=32768,fsync_mode=nobarrier             latemount,wait,check,quota,fileencryption=software,keydirectory=/metadata/vold/metadata_encryption,reservedsize=32M,checkpoint=fs,formattable
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/persist    /mnt/vendor/persist     ext4    noatime,nosuid,nodev,barrier=1,data=ordered,nodelalloc,errors=panic   wait,check,formattable
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/misc       /misc           emmc    defaults                         defaults
+/dev/block/zram0                                             none            swap    defaults                         zramsize=314572800,max_comp_streams=8,zram_backingdev_size=128M
+*/block/mmcblk0                                              auto            auto    defaults                         voldmanaged=sdcard1:auto,encryptable=userdata
+*/block/sd*                                                  auto            auto    defaults                         voldmanaged=usb:auto,noemulatedsd

fstab.am62x.sdcard
@@ -1,0 +1,9 @@
+system                                                       /system         ext4    noatime,ro,errors=panic          wait,slotselect,logical,first_stage_mount
+vendor                                                       /vendor         ext4    noatime,ro,errors=panic          wait,slotselect,logical,first_stage_mount
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/metadata   /metadata       ext4    noatime,nosuid,nodev,discard     wait,check,formattable,first_stage_mount
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/userdata   /data           f2fs    noatime,nosuid,nodev,discard,reserve_root=32768,fsync_mode=nobarrier             latemount,wait,check,quota,fileencryption=software,keydirectory=/metadata/vold/metadata_encryption,reservedsize=32M,checkpoint=fs,formattable
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/persist    /mnt/vendor/persist     ext4    noatime,nosuid,nodev,barrier=1,data=ordered,nodelalloc,errors=panic   wait,check,formattable
+/dev/block/platform/bus@f0000/fa00000.mmc/by-name/misc       /misc           emmc    defaults                         defaults
+/dev/block/zram0                                             none            swap    defaults                         zramsize=314572800,max_comp_streams=8,zram_backingdev_size=128M
+*/block/mmcblk0                                              auto            auto    defaults                         voldmanaged=sdcard1:auto,encryptable=userdata
+*/block/sd*                                                  auto            auto    defaults                         voldmanaged=usb:auto,noemulatedsd

commit 94cd095d9ab945c3ce063343908563b41e338f92

flashall: allow error handling on metadata formatting
Since SD Card boot support update done with
commit 2c5e44142790 ("am62x|am62x-lp: add support of SD Card boot"), we
also exit on error early (via the `set -e` option).

This is un-wanted for metadata formatting, since we have a special error
handling case where we erase metadata instead of formatting it.

Surround "format metadata" with +e/-e to allow error handling in that case.

Fixes: 2c5e44142790 ("am62x|am62x-lp: add support of SD Card boot")
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

flashall.sh
@@ -213,12 +213,14 @@ FDISK_CMDS
 		${FASTBOOT} erase misc
 
 		echo "Formatting metadata partition"
+		set +e
 		${FASTBOOT} format metadata
 		if [ $? -eq 1 ]; then
 			echo "formating failed"
 			${FASTBOOT} erase metadata
 
 		fi
+		set -e
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then