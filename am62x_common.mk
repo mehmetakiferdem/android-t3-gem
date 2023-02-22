2bf6c637b426f7c3380c5f8160a43714909d43b4
Guillaume La Roque <glaroque@baylibre.com>
2023-02-22 15:47:13 +0000

    am62x_common: set some dalvik and pm option
    To fix some CTS test (CtsIcuTestCases, CtsWindowManagerDeviceTestCases,
    CtsOsTestCases, CtsDisplayTestCases ....)
    
    Apply some dalvik and lmk setting from go default prop.
    Add more memory in java heap.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- am62x_common.mk
+++ am62x_common.mk
@@ -1,12 +1,19 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=1m \
-    dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=384m \
+    dalvik.vm.heaptargetutilization=0.90 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=2m \
-    dalvik.vm.usejit=true
+    dalvik.vm.usejit=true \
+    ro.lmk.medium=700 \
+    ro.lmk.critical_upgrade=true \
+    ro.lmk.upgrade_pressure=40 \
+    ro.lmk.downgrade_pressure=60 \
+    ro.lmk.kill_heaviest_task=false \
+    pm.dexopt.downgrade_after_inactive_days=10 \
+    pm.dexopt.shared=quicken
 
 # Set SOC information
 PRODUCT_VENDOR_PROPERTIES += \
