mehmetakiferdem/android-t3-gem

commit 23c674818ab2b2b518f945a761afbc9a5f1aec0f

am62x-lp: Add support of AM62X-LP-SK EVM board
AM62X-LP-SK board was same SoC as AM62X-SK EVM board with different SoC pacakging and
small difference on PCB:
- By default no Wifi, possible with m.2 socket.
- Only one Ethernet.
- Not Same Bootloader binaries.
- different Device tree too.

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

am62x.mk
@@ -15,18 +15,12 @@
 #
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := false
 
+TARGET_WIFI_SUPPORT := true
+TARGET_BL_NAME := am62x-sk
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/ti/am62x/device.mk)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=1m \
-    dalvik.vm.heapgrowthlimit=64m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=2m \
-    dalvik.vm.usejit=false
 
 PRODUCT_NAME := am62x
 PRODUCT_DEVICE := am62x
@@ -35,11 +29,4 @@ PRODUCT_MODEL := AOSP on AM62X EVM
 PRODUCT_MANUFACTURER := Texas_Instruments
 PRODUCT_CHARACTERISTICS := tablet
 
-# Set SOC information
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
-    ro.soc.model=$(PRODUCT_DEVICE)
-
-# clean-up all unknown PRODUCT_PACKAGES
-allowed_list := product_manifest.xml
-$(call enforce-product-packages-exist, $(allowed_list))
+$(call inherit-product, device/ti/am62x/am62x_common.mk)

am62x_lp.mk
@@ -13,23 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-TARGET_WIFI_SUPPORT := true
-TARGET_BL_NAME := am62x-sk
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := false
 
-$(call inherit-product, device/google_car/common/pre_google_car.mk)
-$(call inherit-product, device/ti/am62x/auto/device-car.mk)
-$(call inherit-product, device/google_car/common/post_google_car.mk)
+TARGET_WIFI_SUPPORT := false
+TARGET_BL_NAME := am62x-lp-sk
 
-PRODUCT_PACKAGE_OVERLAYS := device/ti/am62x/auto/overlay
-$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/ti/am62x/device.mk)
 
-PRODUCT_NAME := am62x_car
+PRODUCT_NAME := am62x_lp
 PRODUCT_DEVICE := am62x
 PRODUCT_BRAND := TI
-PRODUCT_MODEL := AOSP Car on AM62X EVM
+PRODUCT_MODEL := AOSP on AM62X-LP EVM
 PRODUCT_MANUFACTURER := Texas_Instruments
+PRODUCT_CHARACTERISTICS := tablet
 
-# Set SOC information
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
-    ro.soc.model=$(PRODUCT_DEVICE)
+$(call inherit-product, device/ti/am62x/am62x_common.mk)

AndroidProducts.mk
@@ -16,10 +16,16 @@
 
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/am62x.mk \
+	$(LOCAL_DIR)/am62x_lp.mk \
 	$(LOCAL_DIR)/auto/am62x_car.mk \
+	$(LOCAL_DIR)/auto/am62x_lp_car.mk
 	
 COMMON_LUNCH_CHOICES := \
 	am62x-userdebug \
 	am62x-user \
 	am62x_car-userdebug \
-	am62x_car-user
+	am62x_car-user \
+	am62x_lp-userdebug \
+	am62x_lp-user \
+	am62x_lp_car-userdebug \
+	am62x_lp_car-user

BoardConfig.mk
@@ -121,9 +121,9 @@ BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Copy Bootloader prebuilts and prebuilts images
 PRODUCT_COPY_FILES += \
-        vendor/ti/am62x/bootloader/am62x-sk/tiboot3.bin:$(TARGET_OUT)/tiboot3.bin \
-        vendor/ti/am62x/bootloader/am62x-sk/tispl.bin:$(TARGET_OUT)/tispl.bin \
-        vendor/ti/am62x/bootloader/am62x-sk/u-boot.img:$(TARGET_OUT)/u-boot.img \
+        vendor/ti/am62x/bootloader/${TARGET_BL_NAME}/tiboot3.bin:$(TARGET_OUT)/tiboot3.bin \
+        vendor/ti/am62x/bootloader/${TARGET_BL_NAME}/tispl.bin:$(TARGET_OUT)/tispl.bin \
+        vendor/ti/am62x/bootloader/${TARGET_BL_NAME}/u-boot.img:$(TARGET_OUT)/u-boot.img \
         vendor/ti/am62x/binaries/persist.img:$(TARGET_OUT)/persist.img \
         device/ti/am62x-kernel/kernel/5.10/dtbo.img:$(TARGET_OUT)/dtbo-unsigned.img
 

am62x_common.mk
@@ -1,0 +1,18 @@
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=1m \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=2m \
+    dalvik.vm.usejit=false
+
+# Set SOC information
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
+    ro.soc.model=$(PRODUCT_DEVICE)
+
+# clean-up all unknown PRODUCT_PACKAGES
+allowed_list := product_manifest.xml
+$(call enforce-product-packages-exist, $(allowed_list))

auto/am62x_lp_car.mk
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-TARGET_WIFI_SUPPORT := true
-TARGET_BL_NAME := am62x-sk
+TARGET_WIFI_SUPPORT := false
+TARGET_BL_NAME := am62x-lp-sk
 
 $(call inherit-product, device/google_car/common/pre_google_car.mk)
 $(call inherit-product, device/ti/am62x/auto/device-car.mk)
@@ -23,10 +23,10 @@ $(call inherit-product, device/google_car/common/post_google_car.mk)
 PRODUCT_PACKAGE_OVERLAYS := device/ti/am62x/auto/overlay
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 
-PRODUCT_NAME := am62x_car
+PRODUCT_NAME := am62x_lp_car
 PRODUCT_DEVICE := am62x
 PRODUCT_BRAND := TI
-PRODUCT_MODEL := AOSP Car on AM62X EVM
+PRODUCT_MODEL := AOSP Car on AM62X-LP EVM
 PRODUCT_MANUFACTURER := Texas_Instruments
 
 # Set SOC information

build/tasks/dtimages.mk
@@ -13,7 +13,8 @@ DTBIMAGE := $(PRODUCT_OUT)/dtb.img
 
 # Please keep this list fixed: add new files in the end of the list
 DTB_FILES := \
-	$(LOCAL_DTB)/k3-am625-sk.dtb
+	$(LOCAL_DTB)/k3-am625-sk.dtb \
+	$(LOCAL_DTB)/k3-am62x-lp-sk.dtb
 
 $(DTBIMAGE): $(DTB_FILES)
 	cat $^ > $@

device.mk
@@ -72,6 +72,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	zygote.critical_window.minute=10
 
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+ifeq ($(TARGET_WIFI_SUPPORT), true)
 # Add wifi-related packages
 PRODUCT_PACKAGES += libwpa_client wpa_supplicant hostapd wificond wpa_cli
 PRODUCT_PROPERTY_OVERRIDES += wifi.interface=wlan0 \
@@ -79,7 +84,14 @@ PRODUCT_PROPERTY_OVERRIDES += wifi.interface=wlan0 \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# Wifi configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
+
+endif
 
 PRODUCT_PACKAGES += \
 	android.hardware.fastboot@1.1 \
@@ -283,15 +295,10 @@ PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
 PRODUCT_PACKAGES += \
         Launcher3QuickStep \
         WallpaperPicker
-
-# Wifi configuration files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
-    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
 
 # Include hardware projects (HALs)
 $(call inherit-product-if-exists, hardware/ti/am62x/am62x.mk)

commit ac72cfc36664d9c2be7a204a0de7995782baf6bf

am62x: fix am62x_common include
In order to set the soc properties (ro.soc.manufacturer, ro.soc.model),
am62x_common.mk should be included. Using inherit-product method should not
be used here.

Fixes: 23c674818ab2 ("am62x-lp: Add support of AM62X-LP-SK EVM board")
Signed-off-by: souajih <souajih@baylibre.com>

am62x.mk
@@ -33,4 +33,4 @@ PRODUCT_MODEL := AOSP on AM62X EVM
 PRODUCT_MANUFACTURER := TexasInstruments
 PRODUCT_CHARACTERISTICS := tablet
 
-$(call inherit-product, device/ti/am62x/am62x_common.mk)
+include device/ti/am62x/am62x_common.mk

am62x_lp.mk
@@ -29,4 +29,4 @@ PRODUCT_MODEL := AOSP on AM62X-LP EVM
 PRODUCT_MANUFACTURER := TexasInstruments
 PRODUCT_CHARACTERISTICS := tablet
 
-$(call inherit-product, device/ti/am62x/am62x_common.mk)
+include device/ti/am62x/am62x_common.mk