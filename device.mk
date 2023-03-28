9c5e9a5b480fa12dddeb7844bbd1a08edb59bcf0
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2023-03-28 19:53:24 +0000

    device: camera: add libcamera cam commandline tool
    This brings in the "cam" commandline utility, which can be used in
    userdebug (using adb root) to capture some frames from the ov5640 CSI
    camera.
    
    This is a debugging tool, hence it's only available in userdebug builds.
    
    To use the camera, make sure to:
    1. Plug it in, hardware wise
    2. Select dtbo_index 2 in U-boot shell with:
       => env set dtbo_index 2
       => saveenv
       => reset
    
    Then, a capture can be taken with:
    
    adb root && adb shell
    am62x:/ # mkdir /data/vendor/hardware/camera
    am62x:/ # cam -c1 -C1 -s pixelformat=YUYV,width=1024,height=768 --file=/data/vendor/hardware/camera/
    
    To view the picture, it can be converted with ffmpeg:
    adb pull /data/vendor/hardware/camera/
    ffmpeg -f rawvideo -s 1024x768 -pix_fmt yuyv422 -i camera/frame-cam0-stream0-000000.bin picture.png
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -329,6 +329,12 @@ PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# CSI Camera using libcamera
+PRODUCT_COPY_FILES += \
+     device/ti/am62x/camera/camera_hal.yaml:$(TARGET_COPY_OUT_VENDOR)/etc/libcamera/camera_hal.yaml
+
+PRODUCT_PACKAGES_DEBUG += cam
+
 PRODUCT_PACKAGES += \
         Launcher3QuickStep \
         WallpaperPicker
