eedbc14aa32b7f3b56c6265fb6041d7ff7a573ac
Safae Ouajih <souajih@baylibre.com>
2023-07-11 21:00:59 +0000

    device: add secure_lock_screen feature
    Add android.software.secure_lock_screen feature
    
    This fixes the following cts tests:
    Module: CtsKeystoreTestCases
    Tests: - android.keystore.cts.CipherTest#testCanCreateAuthBoundKeyWhenScreenLocked
           - android.keystore.cts.CipherTest#testKeyguardLockAndUnlock
    
    Signed-off-by: Safae Ouajih <souajih@baylibre.com>

--- device.mk
+++ device.mk
@@ -260,7 +260,8 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.software.voice_recognizers.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.voice_recognizers.xml \
         frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
         frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml \
-        device/ti/am62x/android.hardware.hardware_keystore.optee-keymaster.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.optee-keymaster.xml
+        device/ti/am62x/android.hardware.hardware_keystore.optee-keymaster.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.optee-keymaster.xml \
+        frameworks/native/data/etc/android.software.secure_lock_screen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.secure_lock_screen.xml
 
 PRODUCT_COPY_FILES += \
         device/ti/am62x/init.am62x.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.am62x.rc \
