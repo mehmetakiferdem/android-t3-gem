fec1b206fd32930fb6bdbe1d1dc761bafad63cae
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2023-11-29 14:33:58 +0000

    am62p: add snagboot recovery yaml and dfu prebuilts
    DFU can be used the same as for AM62X:
    
    $ snagrecover -s am625 -f ./am62px-sk-evm-hsfs.yaml
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- am62p/BoardConfig.mk
+++ am62p/BoardConfig.mk
@@ -38,6 +38,14 @@ PRODUCT_COPY_FILES += \
         vendor/ti/am62x/bootloader/am62px-sk/tiboot3.bin:$(TARGET_OUT)/tiboot3-am62px-sk.bin \
         vendor/ti/am62x/bootloader/am62px-sk/tiboot3-hsfs.bin:$(TARGET_OUT)/tiboot3-am62px-sk-hsfs.bin \
         vendor/ti/am62x/bootloader/am62px-sk/tispl.bin:$(TARGET_OUT)/tispl-am62px-sk.bin \
-        vendor/ti/am62x/bootloader/am62px-sk/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk.img
+        vendor/ti/am62x/bootloader/am62px-sk/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk.img \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/tiboot3.bin:$(TARGET_OUT)/tiboot3-am62px-sk-dfu.bin \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/tiboot3-hsfs.bin:$(TARGET_OUT)/tiboot3-am62px-sk-dfu-hsfs.bin \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/tispl.bin:$(TARGET_OUT)/tispl-am62px-sk-dfu.bin \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk-dfu.img
+
+# Copy snagrecover config file
+PRODUCT_COPY_FILES += \
+        device/ti/am62x/config/dfu/am62px-sk-evm-hsfs.yaml:$(TARGET_OUT)/am62px-sk-evm-hsfs.yaml
 
 include device/ti/am62x/BoardConfig-common.mk
