8e5ad6b337cbe43827d6e9eec6dbc3663b8d96c6
Guillaume La Roque <glaroque@baylibre.com>
2023-11-29 14:33:47 +0000

    am62p: force GPU composition
    To reduce pressure on CPU we force to do composition with GPU instead of
    HW composer.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- am62p/device.mk
+++ am62p/device.mk
@@ -37,4 +37,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.gralloc=am62p \
     ro.opengles.version=196610
 
+# Force GPU for composition instead of HW Composer
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hwc.drm.scale_with_gpu=1
+
 include device/ti/am62x/device-common.mk
