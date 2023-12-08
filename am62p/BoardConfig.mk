mehmetakiferdem/android-t3-gem

commit fec1b206fd32930fb6bdbe1d1dc761bafad63cae

am62p: add snagboot recovery yaml and dfu prebuilts
DFU can be used the same as for AM62X:

$ snagrecover -s am625 -f ./am62px-sk-evm-hsfs.yaml

Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

am62p/BoardConfig.mk
@@ -38,6 +38,14 @@ PRODUCT_COPY_FILES += \
         vendor/ti/am62x/bootloader/am62px-sk/tiboot3.bin:$(TARGET_OUT)/tiboot3-am62px-sk.bin \
         vendor/ti/am62x/bootloader/am62px-sk/tiboot3-hsfs.bin:$(TARGET_OUT)/tiboot3-am62px-sk-hsfs.bin \
         vendor/ti/am62x/bootloader/am62px-sk/tispl.bin:$(TARGET_OUT)/tispl-am62px-sk.bin \
-        vendor/ti/am62x/bootloader/am62px-sk/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk.img
+        vendor/ti/am62x/bootloader/am62px-sk/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk.img \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/tiboot3.bin:$(TARGET_OUT)/tiboot3-am62px-sk-dfu.bin \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/tiboot3-hsfs.bin:$(TARGET_OUT)/tiboot3-am62px-sk-dfu-hsfs.bin \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/tispl.bin:$(TARGET_OUT)/tispl-am62px-sk-dfu.bin \
+        vendor/ti/am62x/bootloader/am62px-sk-dfu/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk-dfu.img
+
+# Copy snagrecover config file
+PRODUCT_COPY_FILES += \
+        device/ti/am62x/config/dfu/am62px-sk-evm-hsfs.yaml:$(TARGET_OUT)/am62px-sk-evm-hsfs.yaml
 
 include device/ti/am62x/BoardConfig-common.mk

config/dfu/am62px-sk-evm-hsfs.yaml
@@ -1,0 +1,6 @@
+tiboot3:
+   path: tiboot3-am62px-sk-dfu-hsfs.bin
+tispl:
+   path: tispl-am62px-sk-dfu.bin
+u-boot:
+   path: u-boot-am62px-sk-dfu.img

commit d1f3546e8a82ad96ae6f1bc737af419744ee4005

am62p: don't copy tiboot3.bin from prebuilts
There is no GP support for AM62P so we don't provide the (GP)
tiboot3.bin.
Only tiboot3-hsfs.bin is provided.

Remove the wrong PRODUCT_COPY_FILES.

Fixes: fec1b206fd32 ("am62p: add snagboot recovery yaml and dfu prebuilts")
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

am62p/BoardConfig.mk
@@ -35,11 +35,9 @@ BOARD_BOOTCONFIG += androidboot.hardware=am62p
 
 # Copy Bootloader prebuilts and prebuilts images
 PRODUCT_COPY_FILES += \
-        vendor/ti/am62x/bootloader/am62px-sk/tiboot3.bin:$(TARGET_OUT)/tiboot3-am62px-sk.bin \
         vendor/ti/am62x/bootloader/am62px-sk/tiboot3-hsfs.bin:$(TARGET_OUT)/tiboot3-am62px-sk-hsfs.bin \
         vendor/ti/am62x/bootloader/am62px-sk/tispl.bin:$(TARGET_OUT)/tispl-am62px-sk.bin \
         vendor/ti/am62x/bootloader/am62px-sk/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk.img \
-        vendor/ti/am62x/bootloader/am62px-sk-dfu/tiboot3.bin:$(TARGET_OUT)/tiboot3-am62px-sk-dfu.bin \
         vendor/ti/am62x/bootloader/am62px-sk-dfu/tiboot3-hsfs.bin:$(TARGET_OUT)/tiboot3-am62px-sk-dfu-hsfs.bin \
         vendor/ti/am62x/bootloader/am62px-sk-dfu/tispl.bin:$(TARGET_OUT)/tispl-am62px-sk-dfu.bin \
         vendor/ti/am62x/bootloader/am62px-sk-dfu/u-boot.img:$(TARGET_OUT)/u-boot-am62px-sk-dfu.img