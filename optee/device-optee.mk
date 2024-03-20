5bb755522641b18df82696a2b54b38cc3b5d667f
Julien Masson <jmasson@baylibre.com>
2024-03-20 15:46:33 +0100

    optee: include optee-client device mk
    optee-client has moved to Android bp format, the default flags are now
    set in optee_client.device.mk
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- optee/device-optee.mk
+++ optee/device-optee.mk
@@ -30,6 +30,9 @@ CFG_TEE_CLIENT_LOAD_PATH := /vendor/lib/
 
 BUILD_OPTEE_MK := device/ti/am62x/optee/build_optee.mk
 
+# optee-client (libteec and tee-supplicant)
+include vendor/linaro/optee_client/optee_client.device.mk
+
 PRODUCT_PACKAGES += \
     libteec \
     tee-supplicant
