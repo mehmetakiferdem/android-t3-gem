83c203e8b856f80859b4a41d6f5acd0b97a9a038
Guillaume La Roque <glaroque@baylibre.com>
2024-03-20 15:46:26 +0100

    am62p: add HW decoding via v4l2_codec2 integration
    //external/v4l2_codec2 provides a Codec2 implementation based on
    standard v4l2 /dev/video nodes.
    
    Add it to am62p by following the instructions from the README.md
    
    Test: dumpsys media.player
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>
    Co-developed-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- am62p/device.mk
+++ am62p/device.mk
@@ -12,4 +12,27 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     device/ti/am62x/am62p/ueventd.am62p.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
+# V4L2 Codec2
+PRODUCT_SOONG_NAMESPACES += external/v4l2_codec2
+
+PRODUCT_COPY_FILES += \
+    device/ti/am62x/am62p/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
+    device/ti/am62x/am62p/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml
+
+# Set the customized property of v4l2_codec2, including:
+# - The maximum concurrent instances for decoder/encoder.
+#   It should be the same as "concurrent-instances" at media_codec_c2.xml.
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.v4l2_codec2.decode_concurrent_instances=32
+
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0-service-v4l2 \
+    libc2plugin_store
+
+# See:
+# https://android.googlesource.com/platform/external/v4l2_codec2/#quick-start-guide
+# for documentation on the bitmask values
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.stagefright.c2-poolmask=0xf50000
+
 include device/ti/am62x/device-common.mk
