c61be12661e5988ff6b44ce150310556106e0d8f
Guillaume La Roque <glaroque@baylibre.com>
2024-05-24 10:33:31 +0200

    common: add quiet in kernel command line
    To reduce log on serial console and reduce boottime add quiet in kernel
    command line.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -136,7 +136,8 @@ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 ifneq ($(TARGET_BUILD_VARIANT), user)
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 endif
-BOARD_KERNEL_CMDLINE += init=/init
+BOARD_KERNEL_CMDLINE += init=/init 
+BOARD_KERNEL_CMDLINE += quiet
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += mem_sleep_default=deep 
 
