mehmetakiferdem/android-t3-gem

commit c73f4ceff265cc087fe9fed5b07f1448368b97c1

all: enable vendor_dlkm partition
Enable support of vendor dlkm partition.

Fix VTS Test: DlkmPartitionTest#VendorDlkmPartition

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

BoardConfig-common.mk
@@ -49,7 +49,8 @@ AB_OTA_PARTITIONS := \
     system \
     vendor \
     vendor_boot \
-    init_boot
+    init_boot \
+    vendor_dlkm
 
 ifeq ($(TARGET_AVB_ENABLE), true)
 AB_OTA_PARTITIONS += vbmeta
@@ -75,6 +76,12 @@ BOARD_SUPER_PARTITION_GROUPS := db_dynamic_partitions
 BOARD_DB_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor
 BOARD_SUPER_PARTITION_SIZE := 4831838208
 BOARD_DB_DYNAMIC_PARTITIONS_SIZE := 2411724800
+
+# Vendor DLKM partition
+BOARD_USES_VENDOR_DLKMIMAGE := true
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+BOARD_DB_DYNAMIC_PARTITIONS_PARTITION_LIST += vendor_dlkm
 
 TARGET_SCREEN_DENSITY ?= 240
 
@@ -259,3 +266,5 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/tps65219-regulator.ko
 
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD +=  $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES)
+
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_MODULES)

config/fstab/fstab.in
@@ -1,5 +1,6 @@
 system                                                       /system         ext4    noatime,ro,errors=panic          wait,slotselect,logical,first_stage_mount
 vendor                                                       /vendor         ext4    noatime,ro,errors=panic          wait,slotselect,logical,first_stage_mount
+vendor_dlkm                                                  /vendor_dlkm    ext4    noatime,ro,errors=panic          wait,slotselect,logical,first_stage_mount
 /dev/block/platform/bus@f0000/@sdio_node@/by-name/vendor_boot /vendor_boot   emmc    defaults                         recoveryonly,slotselect
 /dev/block/platform/bus@f0000/@sdio_node@/by-name/boot       /boot           emmc    defaults                         recoveryonly,slotselect,first_stage_mount
 /dev/block/platform/bus@f0000/@sdio_node@/by-name/init_boot  /init_boot      emmc    defaults                         recoveryonly,slotselect,first_stage_mount

config/fstab/fstab.in.avb
@@ -1,5 +1,6 @@
 system                                                       /system         ext4    ro,barrier=1                     wait,avb=vbmeta,slotselect,logical,first_stage_mount
 vendor                                                       /vendor         ext4    ro,barrier=1                     wait,avb,slotselect,logical,first_stage_mount
+vendor_dlkm                                                  /vendor_dlkm    ext4    noatime,ro,errors=panic          wait,avb,slotselect,logical,first_stage_mount
 /dev/block/platform/bus@f0000/@sdio_node@/by-name/vendor_boot /vendor_boot   emmc    defaults                         recoveryonly,slotselect
 /dev/block/platform/bus@f0000/@sdio_node@/by-name/boot       /boot           emmc    defaults                         recoveryonly,slotselect,first_stage_mount,avb=boot
 /dev/block/platform/bus@f0000/@sdio_node@/by-name/init_boot  /init_boot      emmc    defaults                         recoveryonly,slotselect,first_stage_mount,avb=init_boot

commit 8e4b12c35f6395a321f22cf5e6a23841aebfbc3c

common: avb: add vbmeta_vendor_dlkm to AB_OTA_PARTITIONS
When we enabled vendor_dlkm partition, we forgot to add this partition
to the AB_OTA_PARTITIONS list.

Indeed when we applied an OTA with TARGET_AVB_ENABLE=true, we had the
following errors after switching to slot b:

[    5.788498][    T1] init: Mapped vendor_dlkm_b as snapshot device at /dev/block/dm-8
...
[    6.054363][    T1] init: [libfs_avb] Built verity table: '1 /dev/block/dm-8 /dev/block/dm-8 4096 4096 6621 6621 sha256 ea96abe02b51dcecb50508da6060be8194c208c4e5b69aea3872e1a993a5ea82 a7b084d3b16033de16dda90ac4737e34d419c721879e0abb7f9548d5ef53bd6e 10 use_fec_from_device /dev/block/dm-8 fec_roots 2 fec_blocks 6674 fec_start 6674 restart_on_corruption ignore_zero_blocks'
[    6.088010][    T1] device-mapper: verity: sha256 using implementation "sha256-sa2ul"
[    6.107102][   T71] device-mapper: verity-fec: 254:8: FEC: recursion too deep
[    6.114284][   T71] device-mapper: verity: 254:8: metadata block 6621 is corrupted
[    6.121920][   T71] kvm: exiting hardware virtualization
[    6.135243][   T71] reboot: Restarting system with command 'dm-verity device corrupted'

Fixes: c73f4ceff265 ("all: enable vendor_dlkm partition")
Signed-off-by: Julien Masson <jmasson@baylibre.com>

BoardConfig-common.mk
@@ -56,7 +56,7 @@ AB_OTA_PARTITIONS := \
     vendor_dlkm
 
 ifeq ($(TARGET_AVB_ENABLE), true)
-AB_OTA_PARTITIONS += vbmeta
+AB_OTA_PARTITIONS += vbmeta vbmeta_vendor_dlkm
 endif
 
 # FS Configuration