2adc749f169a1dc9f9221278d618216872d0e8d7
Guillaume La Roque <glaroque@baylibre.com>
2024-08-08 16:02:43 +0200

    BoardConfig-common: move PRUSS driver in vendor_dlkm
    On kernel 6.6 PRUSS driver was not working fine with deferred probe.
    
    So to fix issue move driver in vendor_dlkm partitions instead of ramdisk
    to be sure all deps driver was loaded.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -222,10 +222,8 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/k3-udma.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/k3-udma-glue.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/k3-ringacc.ko \
-        device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/pruss.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/irq-ti-sci-inta.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/irq-ti-sci-intr.ko \
-        device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/irq-pruss-intc.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/ti_sci.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/ti_sci_pm_domains.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/syscon-clk.ko \
@@ -261,7 +259,6 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/phy-gmii-sel.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/phy-j721e-wiz.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/palmas-regulator.ko \
-        device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/pru_rproc.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/optee.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/k3_bandgap.ko \
         device/ti/am62x-kernel/kernel/$(TARGET_KERNEL_USE)/k3_j72xx_bandgap.ko \
