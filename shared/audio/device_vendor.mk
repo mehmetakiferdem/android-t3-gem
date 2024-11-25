ad433942f1fff6a81234c505b9342e41ce382ec0
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2024-11-25 13:33:13 +0100

    common: Update Audio HAL to AIDL
    Replace TinyHAL with the newly developed AIDL Audio HAL which is
    based on the reference implementation from:
    //hardware/interfaces/audio/aidl/default
    
    The new audio HAL includes:
    - primary
    - remote submix
    - usb
    - bluetooth
    - effects
    
    We no longer need to list the hals in the device manifest because it
    comes via a fragment.
    
    Note: Bluetooth audio is mandatory, otherwise the board won't boot.
    
    Test: playback on jack # works, same volume as before
    Test: atest VtsHalAudioCoreTargetTest
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- shared/audio/device_vendor.mk
+++ shared/audio/device_vendor.mk
@@ -1,19 +1,5 @@
-# NOTE: each product should also add audio.primary.$(TARGET_DEVICE) to its PRODUCT_PACKAGES
 PRODUCT_PACKAGES += \
-	audio.r_submix.default \
-	android.hardware.audio.service \
-	android.hardware.audio@6.0-impl \
-	android.hardware.audio.effect@6.0-impl
-
-# Audio USB HAL
-PRODUCT_PACKAGES += \
-	audio.usb.default
-
-PRODUCT_PACKAGES += audio.primary.$(PRODUCT_PLATFORM)
-
-# Audio HAL
-PRODUCT_COPY_FILES += \
-	device/ti/am62x/shared/audio/config/audio_hal_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.$(PRODUCT_PLATFORM).xml
+	com.android.hardware.audio.ti
 
 # audio policy configuration
 USE_XML_AUDIO_POLICY_CONF := 1
@@ -23,7 +9,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-	frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+	frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+	hardware/ti/am62x/audio/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml
 
 
 # Low level audio tools for debugging
