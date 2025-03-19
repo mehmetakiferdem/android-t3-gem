8e1cf2db86e845aefff2b1d9688923b0aa0b2b94
Guillaume La Roque <glaroque@baylibre.com>
2025-03-19 15:59:16 +0100

    bluetooth: disable bluetooth properly
    Disable properly bluetooth and remove blueooth service which are not
    needed anymore.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- shared/bluetooth/device_vendor.mk
+++ shared/bluetooth/device_vendor.mk
@@ -1,5 +1,4 @@
-PRODUCT_PACKAGES += \
-	android.hardware.bluetooth-service.default
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
