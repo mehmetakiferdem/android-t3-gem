761b7072fedc75b86f87ec3243d7f4a18c98ac2d
Guillaume La Roque <glaroque@baylibre.com>
2025-03-19 15:59:22 +0100

    am62p: add boot image profile
    Add boot image profile for AM62PX SoC to reduce boot time.
    
    1: https://source.android.com/docs/core/runtime/boot-image-profiles
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- am62p.mk
+++ am62p.mk
@@ -27,6 +27,10 @@ PRODUCT_MODEL := AOSP on AM62P EVM
 PRODUCT_MANUFACTURER := TexasInstruments
 PRODUCT_CHARACTERISTICS := tablet
 
+# Boot image profiles
+PRODUCT_COPY_FILES +=  device/ti/am62x/shared/boot-profiles/preloaded-classes-am62p:system/etc/preloaded-classes
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := device/ti/am62x/shared/boot-profiles/boot-image-profile-am62p.txt
+
 # Set SOC information
 PRODUCT_VENDOR_PROPERTIES += \
 	ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
