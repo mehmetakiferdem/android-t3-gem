59236b098e27e19b55736591735aecf0dadbaeba
Guillaume La Roque <glaroque@baylibre.com>
2025-10-07 18:52:06 +0200

    am62x: disable boot profiles for Android 16 migration
    Disable boot profiles as they are from Android 15 and will be
    updated once the Android 16 porting is complete.
    
    - Comment out PRODUCT_COPY_FILES for preloaded-classes
    - Comment out PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION
    - Applied to am62x, am62p, and am67a variants
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- am62x.mk
+++ am62x.mk
@@ -28,8 +28,8 @@ PRODUCT_MANUFACTURER := TexasInstruments
 PRODUCT_CHARACTERISTICS := tablet
 
 # Boot image profiles
-PRODUCT_COPY_FILES +=  device/ti/am62x/shared/boot-profiles/preloaded-classes-am62x:system/etc/preloaded-classes
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := device/ti/am62x/shared/boot-profiles/boot-image-profile-am62x.txt
+# PRODUCT_COPY_FILES +=  device/ti/am62x/shared/boot-profiles/preloaded-classes-am62x:system/etc/preloaded-classes
+# PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := device/ti/am62x/shared/boot-profiles/boot-image-profile-am62x.txt
 
 # Set lowram options
 PRODUCT_VENDOR_PROPERTIES += \
